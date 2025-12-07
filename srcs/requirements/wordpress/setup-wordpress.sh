af68eab8a6cb191a66bb1fcd9c467c68d6c110c8
Kenneth Cheong <kennethkhc29@gmail.com>
2025-12-07 08:44:36 +0800

    fix: remove wp db create, check wp is-installed before installing

--- srcs/requirements/wordpress/setup-wordpress.sh
+++ srcs/requirements/wordpress/setup-wordpress.sh
@@ -2,17 +2,16 @@
 
 attempt=0
 max_attempts=10
-until (echo >/dev/tcp/mariadb/3306) >/dev/null 2>&1
-  do
-     attempt=$((attempt + 1))
-     echo "Waiting for mariadb:3306... (attempt $attempt/$max_attempts)"
+until (echo >/dev/tcp/mariadb/3306) >/dev/null 2>&1; do
+   attempt=$((attempt + 1))
+   echo "Waiting for mariadb:3306... (attempt $attempt/$max_attempts)"
 
    if [[ $attempt -ge $max_attempts ]]; then
-      echo "Could not resolve mariadb:3306 after $max_attempts attempts. Exiting."
-      exit 1
+     echo "Could not resolve mariadb:3306 after $max_attempts attempts. Exiting."
+     exit 1
    fi
 
-     sleep 1
+   sleep 1
 done
 
 echo "MariaDB is ready";
@@ -25,25 +24,28 @@ sleep 3;
 if [ ! -f /srv/wordpress/wp-config.php ]; then
    echo "Creating wp-config.php..."
    DB_PASSWORD=mysupersecretuserpassword
-   echo "Password: $DB_PASSWORD"
    wp config create \
-      --force \
       --dbhost=mariadb:3306 \
       --dbname=wordpress_db \
       --dbuser=wordpress_user --dbpass="$DB_PASSWORD"
-   echo "Creating Wordpress database..."
-   wp db create
+else
+   echo "wp-config.php is already set up. Proceeding..."
+fi
+
+if wp core is-installed; then
+   echo "Wordpress is already installed. Proceeding..."
+else
+   echo "Wordpress is not yet installed."
    echo "Installing Wordpress..."
+   # TODO: map kecheong.42.fr to localhost
    wp core install \
       --url="localhost" \
-      --title="WP TEST" \
-      --admin_user="WP_ADMIN" \
-      --admin_password="WP_ADMIN_PASSWORD" \
-      --admin_email="admin@email.com" \
+      --title="Inception!!!" \
+      --admin_user="kecheong4242" \
+      --admin_password="password4242" \
+      --admin_email="kecheong4242@email.com" \
       --skip-email \
       --locale="en_US"
-else
-   echo "Wordpress already set up. Proceeding..."
 fi
 
 exec php-fpm8.4 --nodaemonize
